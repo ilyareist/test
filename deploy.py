18b7ef10161326179ef9bcc94e1d878382fe05f1
reist <reistlin87@yandex.ru>
2018-04-29 09:41:52 +0300

    added

--- deploy.py
+++ deploy.py
@@ -1,13 +1,15 @@
 import time
 import requests
-from .createEC2 import create_ec2
+from createEC2 import create_ec2
 # from .createECSService import *
 # from .createTaskDefinition import createTaskDefinition
 # from .elbv2Services import *
 # from .createECSCluster import createECSCluster
-from .resources import *
+from resources import *
 
 
 # creating EC2 instances
-print('Creating EC2 instances...')
-instances = create_ec2(ec2_client)["Instances"]
+# print('Creating EC2 instances...')
+# instances = create_ec2(ec2_client)["Instances"]
+
+print(access_key)
